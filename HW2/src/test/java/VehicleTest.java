3338e701d68dc6c31f352d046d1033da0730e8b4
Marina Berdnikova <berdni4ka@rambler.ru>
2023-10-24 18:59:08 +0300

    Fix errors

--- HW2/src/test/java/VehicleTest.java
+++ HW2/src/test/java/VehicleTest.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class VehicleTest {
     Car car;
     Motorcycle motorcycle;
